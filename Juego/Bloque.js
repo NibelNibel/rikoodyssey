4eec86e6f02cac552d75086bd22fd6d6ae3219c9
NibelNibel <orinibel7@gmail.com>
2026-02-15 21:54:52 -0500

    update Juego/Bloque.js

--- Juego/Bloque.js
+++ Juego/Bloque.js
@@ -1,17 +1,17 @@
 export class Bloque {
-  constructor(x, y) { // Asegúrate que solo recibe x, y
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.ancho = 100; // Valor fijo
-    this.alto = 100;  // Valor fijo
+    this.ancho = 100; 
+    this.alto = 100;  
     this.solido = true;
   }
 
 
   dibujar(renderizador) {
-  // Dibujamos el cuerpo del bloque
+
   renderizador.rectangulo(this.x, this.y, this.ancho, this.alto, "rgb(180,180,180)");
-  // Dibujamos un borde sutil para que se vea el relieve entre bloques
+
   renderizador.contexto.strokeStyle = "#22223b";
   renderizador.contexto.lineWidth = 0;
   renderizador.contexto.strokeRect(this.x, this.y, this.ancho, this.alto);
