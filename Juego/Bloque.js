71ea3f95eaae04cf388e2e6bf74429d0d0990350
NibelNibel <orinibel7@gmail.com>
2026-02-17 01:03:52 -0500

    update Juego/Bloque.js

--- Juego/Bloque.js
+++ Juego/Bloque.js
@@ -1,11 +1,13 @@
 export class Bloque {
-  constructor(x, y) {
+  constructor(x, y, recursos) {
+    this.recursos = recursos;
     this.x = x;
     this.y = y;
     this.ancho = 100; 
     this.alto = 100;  
     this.solido = true;
     this.tierra = 0;
+    this.imagen = this.recursos.obtenerImagen(`tierra${this.tierra}`);
   }
 
 
@@ -16,5 +18,6 @@ export class Bloque {
   renderizador.contexto.strokeStyle = "#22223b";
   renderizador.contexto.lineWidth = 1;
   renderizador.contexto.strokeRect(this.x, this.y, this.ancho, this.alto);
+  renderizador.dibujarImagen(this.imagen, this.x - 50, this.y - 50, 200, 200);
 }
 }
