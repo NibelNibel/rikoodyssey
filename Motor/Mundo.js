678e473520409fdd2a29137c60ca21f4df0a24ed
NibelNibel <orinibel7@gmail.com>
2026-02-18 22:06:27 -0500

    update Motor/Mundo.js

--- Motor/Mundo.js
+++ Motor/Mundo.js
@@ -33,9 +33,13 @@ export class Mundo {
   }
 
   agregar(objeto, solido = false) {
+    objeto.solido = solido;
+
     (solido ? this.solidos : this.otros).push(objeto);
+
     const a = this.obtenerCelda(objeto.x, objeto.y);
     const b = this.obtenerCelda(objeto.x + objeto.ancho, objeto.y + objeto.alto);
+
     for (let f = a.f; f <= b.f; f++) {
       for (let c = a.c; c <= b.c; c++) {
         this.cuadricula[f][c].push(objeto);
@@ -43,28 +47,19 @@ export class Mundo {
     }
   }
 
-  preRenderizar(renderizador) {
-    this.mapaCanvas.width = this.ancho;
-    this.mapaCanvas.height = this.alto;
-
-    const ctxOriginal = renderizador.contexto;
-    renderizador.contexto = this.mapaCtx;
-
-    for (const o of this.solidos) {
-      o.dibujar(renderizador);
+  eliminarDeCuadricula(objeto) {
+    for (let f = 0; f < this.filas; f++) {
+      for (let c = 0; c < this.columnas; c++) {
+        const index = this.cuadricula[f][c].indexOf(objeto);
+        if (index !== -1) this.cuadricula[f][c].splice(index, 1);
+      }
     }
-    
-    for (const o of this.decorativos) {
-      renderizador.dibujarImagen(o.imagen, o.x, o.y, o.ancho, o.alto);
-    }
-
-    renderizador.contexto = ctxOriginal;
-    this.mapaRenderizado = true;
   }
 
   verificarRecoleccion(entidad) {
     for (let i = this.otros.length - 1; i >= 0; i--) {
       const objeto = this.otros[i];
+
       if (
         entidad.x < objeto.x + objeto.ancho &&
         entidad.x + entidad.ancho > objeto.x &&
@@ -79,72 +74,103 @@ export class Mundo {
     return null;
   }
 
-  eliminarDeCuadricula(objeto) {
-    for (let f = 0; f < this.filas; f++) {
-      for (let c = 0; c < this.columnas; c++) {
-        const index = this.cuadricula[f][c].indexOf(objeto);
-        if (index !== -1) this.cuadricula[f][c].splice(index, 1);
-      }
-    }
-  }
-
   colisionar(entidad, eje, delta) {
+
     const a = this.obtenerCelda(entidad.x, entidad.y);
     const b = this.obtenerCelda(entidad.x + entidad.ancho, entidad.y + entidad.alto);
+
     const candidatos = new Set();
+
     for (let f = a.f; f <= b.f; f++) {
       for (let c = a.c; c <= b.c; c++) {
         this.cuadricula[f][c].forEach(o => candidatos.add(o));
       }
     }
+
     for (const o of candidatos) {
       if (!o.solido) continue;
+
       if (
         entidad.x < o.x + o.ancho &&
         entidad.x + entidad.ancho > o.x &&
         entidad.y < o.y + o.alto &&
         entidad.y + entidad.alto > o.y
       ) {
+
         if (eje === "x") {
+
+          // Rebote de dash
+          if (entidad.estaDasheando) {
+            entidad.estaDasheando = false;
+            entidad.vx *= -0.4;
+          }
+
           entidad.x -= delta;
+          return o;
+
         } else {
+
           entidad.y -= delta;
-          if (delta > 0) entidad.enSuelo = true;
+
+          if (delta > 0) {
+            entidad.enSuelo = true;
+          }
+
           entidad.vy = 0;
+          return o;
         }
+      }
+    }
+
+    return null;
+  }
+
+  colisionDetectar(entidad) {
+    const a = this.obtenerCelda(entidad.x, entidad.y);
+    const b = this.obtenerCelda(entidad.x + entidad.ancho, entidad.y + entidad.alto);
+
+    const candidatos = new Set();
+
+    for (let f = a.f; f <= b.f; f++) {
+      for (let c = a.c; c <= b.c; c++) {
+        this.cuadricula[f][c].forEach(o => candidatos.add(o));
+      }
+    }
+
+    for (const o of candidatos) {
+      if (!o.solido) continue;
+
+      if (
+        entidad.x < o.x + o.ancho &&
+        entidad.x + entidad.ancho > o.x &&
+        entidad.y < o.y + o.alto &&
+        entidad.y + entidad.alto > o.y
+      ) {
         return o;
       }
     }
+
     return null;
-  } 
-  
-  colisionDetectar(entidad) {
-  const a = this.obtenerCelda(entidad.x, entidad.y);
-  const b = this.obtenerCelda(entidad.x + entidad.ancho, entidad.y + entidad.alto);
-
-  const candidatos = new Set();
-
-  for (let f = a.f; f <= b.f; f++) {
-    for (let c = a.c; c <= b.c; c++) {
-      this.cuadricula[f][c].forEach(o => candidatos.add(o));
-    }
   }
 
-  for (const o of candidatos) {
-    if (!o.solido) continue;
+  preRenderizar(renderizador) {
+    this.mapaCanvas.width = this.ancho;
+    this.mapaCanvas.height = this.alto;
 
-    if (
-      entidad.x < o.x + o.ancho &&
-      entidad.x + entidad.ancho > o.x &&
-      entidad.y < o.y + o.alto &&
-      entidad.y + entidad.alto > o.y
-    ) {
-      return o;
+    const ctxOriginal = renderizador.contexto;
+    renderizador.contexto = this.mapaCtx;
+
+    for (const o of this.solidos) {
+      o.dibujar(renderizador);
     }
-  }
 
-  return null;
-}
+    for (const o of this.decorativos) {
+      renderizador.dibujarImagen(o.imagen, o.x, o.y, o.ancho, o.alto);
+    }
+
+    renderizador.contexto = ctxOriginal;
+    this.mapaRenderizado = true;
+  }
 
   dibujar(renderizador, camara) {
     if (this.mapaRenderizado) {
